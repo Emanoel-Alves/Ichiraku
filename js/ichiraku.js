6ec9dac3599557dcf4a57e5e3cd497855b218166
lais <laiscrisss@alu.ufc.br>
2020-09-07 20:20:50 -0300

    Tela de login e cadastro completa

--- js/ichiraku.js
+++ js/ichiraku.js
@@ -1,19 +1,27 @@
 function entrar(){
-    document.getElementById("logar").onclick = function () {
-        location.href = "file:///C:/Users/srtal/OneDrive/%C3%81rea%20de%20Trabalho/UFC%202020.1/Desenvolvimento%20WEB/Ichiraku-master/Ichiraku-master/pages/home.html";
-    }
-};
 
-function validar(){ 
-    var email = document.getElementById("email");
-    var senha = document.getElementById("senha");
-      
-    if(email == "exemplo@email.com" &&  senha=="11111111"){
-      location.href='home.html';
-      alert('Tudo certo');
-      
-    }else{
-      alert('Usuario ou senha incorretos');
-     
-    }
-    };
+    var senha = document.getElementById("senha").value;
+
+        if( document.forms[0].email.value==""
+        || document.forms[0].email.value.indexOf('@')==-1 
+        || document.forms[0].email.value.indexOf('.')==-1 
+        || senha.length < 8) 
+        { 
+
+                 alert( "Por favor, informe um E-MAIL válido ou uma senha!" ); 
+
+        }
+        
+        else {
+            document.getElementById("logar").onclick = function () {
+            location.href = "file:///C:/Users/srtal/OneDrive/%C3%81rea%20de%20Trabalho/UFC%202020.1/Desenvolvimento%20WEB/Ichiraku-master/Ichiraku-master/pages/home.html";
+        
+            
+        }
+
+        
+
+}
+
+
+};
