43a81a27d208863a8d8ad9736880869a77c16249
lais <laiscrisss@alu.ufc.br>
2020-09-08 15:59:00 -0300

    telas login e cadastro terminadas

---
+++ js/telaCadastroIchiraku.js
@@ -0,0 +1,27 @@
+function cadastrar(){
+
+    var senha = document.getElementById("senha").value;
+
+        if( document.forms[0].email.value==""
+        || document.forms[0].email.value.indexOf('@')==-1 
+        || document.forms[0].email.value.indexOf('.')==-1 
+        || senha.length < 8) 
+        { 
+
+                 alert( "Por favor, informe um E-MAIL válido ou uma senha!" ); 
+
+        }
+        
+        else {
+            document.getElementById("cad").onclick = function () {
+            location.href = "ichiraku.html";
+        
+            
+        }
+
+        
+
+}
+
+
+};
