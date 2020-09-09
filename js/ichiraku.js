d82449dc0789b306779e6b974428b67a0acb514f
Emanoel-Alves <emanoel-alves09@hotmail.com>
2020-09-09 11:39:48 -0300

    Corrigindo link da tela de login para a home

--- js/ichiraku.js
+++ js/ichiraku.js
@@ -1,27 +1,16 @@
-function entrar(){
+function entrar() {
+  var senha = document.getElementById("senha").value;
 
-    var senha = document.getElementById("senha").value;
-
-        if( document.forms[0].email.value==""
-        || document.forms[0].email.value.indexOf('@')==-1 
-        || document.forms[0].email.value.indexOf('.')==-1 
-        || senha.length < 8) 
-        { 
-
-                 alert( "Por favor, informe um E-MAIL válido ou uma senha!" ); 
-
-        }
-        
-        else {
-            document.getElementById("logar").onclick = function () {
-            location.href = "file:///C:/Users/srtal/OneDrive/%C3%81rea%20de%20Trabalho/UFC%202020.1/Desenvolvimento%20WEB/Ichiraku-master/Ichiraku-master/pages/home.html";
-        
-            
-        }
-
-        
-
+  if (
+    document.forms[0].email.value == "" ||
+    document.forms[0].email.value.indexOf("@") == -1 ||
+    document.forms[0].email.value.indexOf(".") == -1 ||
+    senha.length < 8
+  ) {
+    alert("Por favor, informe um E-MAIL válido ou uma senha!");
+  } else {
+    document.getElementById("logar").onclick = function () {
+      location.href = "home.html";
+    };
+  }
 }
-
-
-};
