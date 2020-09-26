25a8d9061d1e0086a45b674344ef277fdac3af79
Emanoel-Alves <emanoel-alves09@hotmail.com>
2020-09-26 12:36:21 -0300

    Adicionando projeto Vue Ichiraku

---
+++ ichiraku-front-end/src/router/index.js
@@ -0,0 +1,55 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../components/Home.vue";
+import CadastroProduto from "../components/CadastroProduto.vue";
+import InformacoesPrato from "../components/InformacoesPrato.vue";
+import OpcoesDoDia from "../components/OpcoesDoDia.vue";
+import TelaLogin from "../components/TelaLogin.vue";
+import TelaCadastro from "../components/TelaCadastro.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "*",
+    component: Home,
+  },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/cadastro-produto",
+    name: "CadastroProduto",
+    component: CadastroProduto,
+  },
+  {
+    path: "/informacoes-prato",
+    name: "InformacoesPrato",
+    component: InformacoesPrato,
+  },
+  {
+    path: "/opcoes-do-dia",
+    name: "OpcoesDoDia",
+    component: OpcoesDoDia,
+  },
+  {
+    path: "/login",
+    name: "TelaLogin",
+    component: TelaLogin,
+  },
+  {
+    path: "/cadastro",
+    name: "TelaCadastro",
+    component: TelaCadastro,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
