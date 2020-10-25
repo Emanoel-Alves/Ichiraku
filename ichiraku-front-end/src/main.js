ff09a93b3b7656072734a9dff6cf1b3977c1ab08
Emanoel-Alves <emanoel-alves09@hotmail.com>
2020-10-25 18:14:49 -0300

    Adicionando projeto back-end com Spring Boot

--- ichiraku-front-end/src/main.js
+++ ichiraku-front-end/src/main.js
@@ -2,13 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import VueSession from "vue-session";
 
 Vue.config.productionTip = false;
 Vue.prototype.$http = axios;
+Vue.use(VueSession);
 
 new Vue({
   router,
-  render: function(h) {
+  render: function (h) {
     return h(App);
   },
 }).$mount("#app");
